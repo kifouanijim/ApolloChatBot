8b6bb3c0e07af303981d3477d90a4a949b387b39
jim <jimkifouani0@gmail.com>
2026-03-05 14:31:16 +0100

    nex

--- app/api/chatbot/route.ts
+++ app/api/chatbot/route.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from "../../../generated/prisma";
 
 // ✅ 1. Gestion du Singleton Prisma (Correctement typé pour éviter les erreurs TS)
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma = globalForPrisma.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 // ✅ Type FAQ
